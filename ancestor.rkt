fd9f0f044b64db1f0cad9e65221d5fb20bfb7197
Sam Tobin-Hochstadt <samth@racket-lang.org>
2012-09-01 16:29:39 -0400

    Fix ancestor

--- ancestor.rkt
+++ ancestor.rkt
@@ -3,15 +3,20 @@
 Semi-literate programming works for datalog as well
 
 This is the data portion -- we just declare some basic facts.
-> parent(john, douglas)
+> parent(john, douglas).
+> parent(mary, john).
 
 The `ancestor` relation computes the transitive closure of the 
 `parent` relation.
 
 > ancestor(A, B) :-
->   parent(A, B)
+>   parent(A, B).
 > ancestor(A, B) :-
 >   parent(A, C),
->   ancestor(C, B)
+>   ancestor(C, B).
 
-And that's all she wrote.
+Now we can ask a question.
+
+> ancestor(mary,A)?
+
+And then we get answers.
