0ae8f31920d72a595f66c6f2e3121e2ea004f6a9
Sam Tobin-Hochstadt <samth@racket-lang.org>
2012-09-01 15:02:06 -0400

    It works.

--- main.rkt
+++ main.rkt
@@ -8,17 +8,48 @@
                        [sl-get-info get-info]))
 
   (define (wrap-read p)
-    (lambda (in)
-      (p (filter-port in))))
+    (lambda (in . args)
+      (apply p (filter-port in) args)))
 
   (define (wrap-read-syntax p)
-    (lambda (n in)
-      (p n (filter-port in))))
+    (lambda (n in . args)
+      (apply p n (filter-port in) args)))
+
+  (define <-byte 62)
 
   (define (filter-port in)
     (make-input-port 
      (cons 'semilit (object-name in))
-     (lambda (...) ...)
+     (let ([remain #f] [remain-start 0])
+       (lambda (bs) 
+         (define len (bytes-length bs))
+         (eprintf ">>> bs-len ~a\n" len)
+         (let outer ()
+           (cond [(and remain (<= (- (bytes-length remain) remain-start) len))
+                  (eprintf ">> more to go but it all fits\n")
+                  (bytes-copy! bs 0 remain remain-start)
+                  (begin0
+                      (- (bytes-length remain) remain-start)
+                    (set! remain-start 0) 
+                    (set! remain #f))]
+                 [remain
+                  (eprintf ">> more to go and it won't fit\n")
+                  (bytes-copy! bs 0 remain remain-start (+ remain-start len))
+                  (set! remain-start (+ remain-start len)) 
+                  len]
+                 [else
+                  (let inner ()
+                    (define line (read-bytes-line in))
+                    (eprintf ">>> line ~a\n" line)
+                    (cond 
+                      [(eof-object? line) line]
+                      [(zero? (bytes-length line))
+                       (bytes-copy! bs 0 (string->bytes/utf-8 "\n"))
+                       1]
+                      [(equal? (subbytes line 0 1) (string->bytes/utf-8 ">"))
+                       (set! remain line) (set! remain-start 1)
+                       (outer)]
+                      [else (inner)]))]))))
      #f ;; peek
      (lambda () (close-input-port in))))
 
---
+++ test.rkt
@@ -0,0 +1,13 @@
+#lang semilit racket
+
+> (define x
+
+
+> (+ 3 4)
+some text here
+
+> )
+
+this is not parsed > at all
+
+> x
